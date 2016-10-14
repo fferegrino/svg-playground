0451cb974d7514a6af73c834097ea3e88391a014
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-10-13 19:20:10 -0500

    Init

---
+++ SvgPlayground/App.cs
@@ -0,0 +1,95 @@
+﻿using NControl.Abstractions;
+using NGraphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SvgPlayground
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "SvgPlayground",
+                Content = new NControlView
+                {
+                    DrawingFunction = (canvas, rect) =>
+                    {
+                        var boxLength = Math.Min(rect.Size.Width, rect.Size.Height);
+                        var newY = Math.Max(rect.Size.Height, rect.Size.Width) / 2 - boxLength / 2;
+                        var box = new Rect(rect.X, newY, boxLength, boxLength);
+                        var innerBorder = box.Resize(-35);
+                        var rectInsideInnerBorder = innerBorder.RectInsideEllipse();
+                        var innerInnerBorder = box.Resize(-200);
+
+                        var point = box.GetCenter();
+
+                        var amarillo = NGraphics.Color.FromRGB(16706817);
+                        var azul = NGraphics.Color.FromRGB(665162);
+
+                        var lineSize = 25;
+
+                        canvas.FillEllipse(box, amarillo); // base
+                        canvas.DrawEllipse(innerBorder, azul, lineSize);
+
+                        canvas.DrawLine(innerInnerBorder.TopLeft, rectInsideInnerBorder.TopLeft, azul, lineSize);
+                        canvas.DrawLine(innerInnerBorder.TopRight, rectInsideInnerBorder.TopRight, azul, lineSize);
+                        canvas.DrawLine(innerInnerBorder.BottomLeft, rectInsideInnerBorder.BottomLeft, azul, lineSize);
+                        canvas.DrawLine(innerInnerBorder.BottomRight, rectInsideInnerBorder.BottomRight, azul, lineSize);
+
+                        
+                        var sizeH = new NGraphics.Size(innerInnerBorder.Width, innerInnerBorder.Height / 2);
+                        var sizeV = new NGraphics.Size(innerInnerBorder.Width / 2, innerInnerBorder.Height);
+                        canvas.DrawPath(
+                            new PathOp[]
+                            {
+                                new MoveTo(innerInnerBorder.TopLeft),
+                                new ArcTo( sizeH ,false, true, innerInnerBorder.TopRight),
+
+                                new MoveTo(innerInnerBorder.TopRight),
+                                new ArcTo( sizeV ,false, false, innerInnerBorder.BottomRight),
+
+                                new MoveTo(innerInnerBorder.BottomRight),
+                                new ArcTo( sizeH ,false, true, innerInnerBorder.BottomLeft),
+
+                                new MoveTo(innerInnerBorder.BottomLeft),
+                                new ArcTo( sizeV ,false, false, innerInnerBorder.TopLeft),
+
+                                new MoveTo(innerInnerBorder.TopLeft),
+                                new ClosePath ()
+                            },
+                            new Pen(azul, lineSize));
+
+
+                        var megaEllipse = box.Resize(1000);
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SvgPlayground/NGraphicsExtensions.cs
@@ -0,0 +1,48 @@
+﻿using NGraphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgPlayground
+{
+    public static class NGraphicsExtensions
+    {
+        public static Rect Resize(this Rect rect, double size)
+        {
+
+            return rect.Resize(size, size);
+        }
+        public static Rect Resize(this Rect rect, double sizeX, double sizeY)
+        {
+            var originalX = rect.X;
+            var originalY = rect.Y;
+            var newX = originalX + (-1 * sizeX);
+            var newY = originalY + (-1 * sizeY);
+            return new Rect(newX, newY, rect.Width + (sizeX * 2), rect.Height + (sizeY * 2));
+        }
+
+        public static Rect RectInsideEllipse(this Rect rect)
+        {
+            var a = rect.Width;
+            var b = rect.Height;
+
+            var newWidth = (a * Math.Sqrt(2)) / 2;
+            var newHeight = (b * Math.Sqrt(2)) / 2;
+
+            var diffW = newWidth - a;
+            var diffH = newHeight - b;
+
+            return rect.Resize(diffW / 2, diffH / 2);
+        }
+
+        public static Point GetCenter(this Rect rect)
+        {
+            var originalX = rect.X;
+            var originalY = rect.Y;
+
+            return new Point(originalX + (rect.Width / 2), originalY + (rect.Height / 2));
+        }
+    }
+}
